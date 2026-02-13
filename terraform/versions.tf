d2b8886ebea62f74f95ba7ca30f93762aff3f8b5
mdproctor0 <mproctor.cyber@gmail.com>
2026-02-13 09:59:41 -0500

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -14,10 +14,14 @@ Rationale:
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
+   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.100"
     }
-  }
+  
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
 }
